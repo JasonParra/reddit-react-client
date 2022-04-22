f14c78c4e32d8fe2237d75443f8fbf7a13e4078f
unknown <jason.parra@endurance.com>
2022-04-21 23:29:35 -0400

    changing  missing tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import PostList from "./pages/PostList/PostList";
 import PostDetail from './pages/PostDetail/PostDetail';
